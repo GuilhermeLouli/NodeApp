30a69fd9a05d0a2e02e427b995fe69462391b16b
Guilherme Louli <guilhermeloulisoares@gmail.com>
2022-04-11 11:58:40 -0300

    Editing product data

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,6 @@ router.get('/add-product', adminController.getAddProduct); // /admin/add-product
 router.get('/products', adminController.getProducts); // /admin/products => GET
 router.post('/add-product', adminController.postAddProduct); // /admin/add-product => POST
 router.get('/edit-product/:productId', adminController.getEditProduct);
-router.get('/edit-product/:productId', adminController.getEditProduct);
-router.post('/edit-product');
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
