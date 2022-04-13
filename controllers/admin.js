b9f6ce38087ca527e2bba811e36edf6c31a9380e
Guilherme Louli <guilhermeloulisoares@gmail.com>
2022-04-13 10:35:04 -0300

    Fetching admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,13 +65,15 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products' //highlight navigation item
-    });
-  });
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products' //highlight navigation item
+      });
+    })
+    .catch(err => { console.log(err) });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
